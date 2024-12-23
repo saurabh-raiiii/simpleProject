1546042b09a4c7d1b15c828ff0afe237a24dfd32
saurabh-raiiii <srai.code@gmail.com>
2024-12-23 16:31:27 +0530

    add-checkPrime-cpp

---
+++ checkPrime.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int i = 2;
+    if(i == 0 || i == 1){
+        cout << 
+    }
+    return 0;
+}
---
+++ oddEven.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+mian(){
+    int n;
+    cin << n;
+    if(n % 2) cout << "it is Odd";
+    else cout << "it is even"
+    return 0;
+}
