276c3307cb994c79eb0c5c8c9b54ae32c2c9b32c
saurabh-raiiii <srai.code@gmail.com>
2024-12-23 16:33:59 +0530

    fixedError

--- checkPrime.cpp
+++ checkPrime.cpp
@@ -4,9 +4,19 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    int i = 2;
-    if(i == 0 || i == 1){
-        cout << 
+    if(n == 0 || n == 1){
+        cout << "it is not a Prime";
+    }
+    else{
+        int i = 2;
+        while(i < n){
+            if(n % i == 0){
+                break;
+            }
+            i += 1;
+        }
+        if(i == n) cout << "it is not a Prime";
+        else cout << "it is a Prime";
     }
     return 0;
 }
